1788eee1e3df4a4b09c918f78fccadd7c788ca3a
azhukovva <zhukovaarina509@gmail.com>
2024-11-19 11:49:05 +0100

    merge

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+       'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -55,7 +56,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ROOT_URLCONF = "marketplace.urls"
@@ -124,6 +125,9 @@ TIME_ZONE = "Europe/Prague"
 USE_I18N = True
 
 USE_TZ = True
+
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
 
 
 # Static files (CSS, JavaScript, Images)
