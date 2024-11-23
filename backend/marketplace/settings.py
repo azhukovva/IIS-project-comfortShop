983315288b1abeb0121e954c4c69320615cb3eb3
Nataliia Solomatina <solomatinanataliia@gmail.com>
2024-11-23 12:20:41 +0100

    changes

--- backend/marketplace/settings.py
+++ backend/marketplace/settings.py
@@ -138,6 +138,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ["rest_framework.authentication.TokenAuthentication"],
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
